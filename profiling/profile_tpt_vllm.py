e7501fc0c74f9be892819f8234d878c309c9c76d
fruitea <aetiurf@gmail.com>
2025-12-10 16:15:16 -0500

    feat: add suffix decoding baseline

--- profiling/profile_tpt_vllm.py
+++ profiling/profile_tpt_vllm.py
@@ -1,46 +1,49 @@
 # %%
 import os
+import sys
 import time
 import openai
+import argparse
+import logging
 from openai import OpenAI
-from datasets import load_dataset, load_from_disk
+
+sys.path.insert(1, os.path.dirname(os.getcwd()) + "/..")
+from utils import populate_dataset, format_problem_and_options, get_first_user_msg
 
 """
 VLLM_USE_V1=0 vllm serve Qwen/Qwen2.5-7B-Instruct --dtype auto -tp 2 --max_model_len 8192 --gpu-memory-utilization 0.8 --port 30000
 """
 
-port = 30000
+parser = argparse.ArgumentParser(description="Profiles the vLLM TPT of a model on all datasets.")
+parser.add_argument("--port", type=int, default=30000, help="Port to use for the vLLM server")
+parser.add_argument("--output_file", type=str, help="Name of the output file")
+parser.add_argument("--target_model_name", type=str, default="Qwen/Qwen2.5-32B-Instruct", 
+                    help="Name of the base model to use")
+parser.add_argument("--num_questions", type=int, default=1,
+                    help="Number of questions to run profiling on")
+parser.add_argument("--max_new_tokens", type=int, default=1024,
+                    help="Max new tokens from the target model")
+parser.add_argument("--log_level",
+                    type=str,
+                    default="INFO",
+                    choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+                    help="Set the logging level")
+args, _ = parser.parse_known_args()
+logging.basicConfig(
+    level=getattr(logging, args.log_level),
+    format="[%(asctime)s %(levelname)s] %(message)s",
+    datefmt="%m%d %H:%M:%S",
+    # datefmt="%m%d",
+)
+
 client = OpenAI(
     api_key="EMPTY",
-    base_url=f"http://localhost:{port}/v1",
+    base_url=f"http://localhost:{args.port}/v1",
 )
-models = client.models.list()
-model = models.data[0].id
 
-system_prompt = """
-Solve the following math problem efficiently and clearly. Please reason step by step, 
-separate logical reasoning steps with two newline characters (\n\n), and put your final answer within \\boxed{{}}.
-Problem: {problem}
-"""
-
-for dataset_name in ["aime", "math", "gpqa"]:
-
-    if dataset_name == "aime":
-        dataset = load_dataset("HuggingFaceH4/aime_2024")["train"]
-    elif dataset_name == "math":
-        dataset = load_dataset("HuggingFaceH4/MATH-500")["test"]
-    elif dataset_name == "gpqa":
-        if os.getenv("HF_HUB_OFFLINE", "0") == "1":
-            dataset = load_from_disk("/scratch/gpfs/RAVIAN/rp2773/hf_cache/datasets/gpqa")
-        else:    
-            dataset = load_dataset("Idavidrein/gpqa", "gpqa_diamond")["train"]
-    else:
-        raise NotImplementedError
-        
-        
-
-
-
+for dataset_name in ["aime", "math", "gpqa", "mmlu", "humaneval"]:
+    args.dataset_name = dataset_name
+    dataset = populate_dataset(args)
 
     # %%
     total_time = 0
@@ -48,31 +51,18 @@ for dataset_name in ["aime", "math", "gpqa"]:
 
     # for problem_id in range(1):
     for problem_id in range(30):
-        if dataset_name == "aime":
-            problem = dataset["problem"][problem_id]
-            options = None
-        elif dataset_name == "math":
-            problem = dataset["problem"][problem_id]
-            options = None
-        elif dataset_name == "gpqa":
-            problem = dataset["Question"][problem_id]
-            options = {
-                "A": dataset["Correct Answer"][problem_id],
-                "B": dataset["Incorrect Answer 1"][problem_id],
-                "C": dataset["Incorrect Answer 2"][problem_id],
-                "D": dataset["Incorrect Answer 3"][problem_id],
-            }
+        raw_data = format_problem_and_options(args, problem_id)
         
         messages = [
-            {"role": "user", "content": system_prompt.format(problem=problem)},
+            {"role": "user", "content": get_first_user_msg(args, raw_data)},
         ]
         extra_body = {"add_generation_prompt": True}
         
         start_time = time.perf_counter()
         response = client.chat.completions.create(
-            model=model,
+            model=args.target_model_name,
             messages=messages,
-            max_tokens=512,
+            max_tokens=args.max_new_tokens,
             extra_body=extra_body,
         )
         num_output_tokens = response.usage.completion_tokens
